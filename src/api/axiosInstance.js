202e87673fecdeebf33f759cc43a5fc171113e87
Adinath Pawar <adinathpawar2858@gmail.com>
2026-08-08 12:17:34 +0530

    https://github.com/Roshni-More/Bid-Management-React-Project.git

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://localhost:44362",
+  baseURL: "https://localhost:7296",
   headers: {
     "Content-Type": "application/json",
   },
